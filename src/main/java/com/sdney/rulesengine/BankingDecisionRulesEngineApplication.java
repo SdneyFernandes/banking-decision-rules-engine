c3312936bba518b57f2c86e1e823ff66ac95e5b3
Sdney_Fernandes <fsidney987@gmail.com>
2026-09-14 02:43:50 -0300

    chore: bootstrap banking decision rules engine

---
+++ src/main/java/com/sdney/rulesengine/BankingDecisionRulesEngineApplication.java
@@ -0,0 +1,13 @@
+package com.sdney.rulesengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingDecisionRulesEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingDecisionRulesEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdney/rulesengine/BankingDecisionRulesEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdney.rulesengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDecisionRulesEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
